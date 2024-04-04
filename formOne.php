a4c5879957260af87b8b7678f3cdf0ea00e9d7a6
venkatesh prasad <prasad99vicky@gmail.com>
2024-04-04 15:00:58 +0530

    PHP Functions

---
+++ formOne.php
@@ -0,0 +1,20 @@
+<!-- //<?php require('./Form/functions.php')?> -->
+   <main>
+    <div class="form-group">
+        <form>
+            <label for="">
+                <textarea name="venkat">
+       
+               
+                PHP is a general-purpose scripting language geared towards web development.[8] It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1993 and released in 1995.[9][10] The PHP reference implementation is now produced by the PHP Group.[11] PHP was originally an abbreviation of Personal Home Page,[12][13] but it now stands for the recursive initialism PHP: Hypertext Preprocessor.[14]
+                
+                </textarea>
+                <?php if (isset($errors['body'])) ?>
+            </label>
+
+   
+    </form>
+    </div>
+</main>
+
+ 
